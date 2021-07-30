aa076f4c74ad11ceac90f0f85be8ea24a53f71f0
ss820938ss <ss820938ss@gmail.com>
2021-07-30 16:51:58 +0900

    2021 07 30 판다스수업2

--- 15.py
+++ 15.py
@@ -30,32 +30,19 @@ element.send_keys(test, Keys.ENTER)
 time.sleep(5)
 driver.find_element_by_class_name('_2Mc8_').send_keys(Keys.ENTER)
 
-# 조회수 찾아내기(안되고있음 ....너무안되고있음...주소전혀못찾고있음...)
-req = requests.get('https://unsplash.com/photos/{}')
-html = req.text
-soup = BeautifulSoup(html, 'html.parser')
+image_tag = driver.find_elements_by_class_name('Q4LuWd')
 
-view = soup.select('')
+time.sleep(1)
 
-result = soup.find_all('td', 'title')
-for i in range(len(result)):
-    print('{:2} 위 : {}'.format(i + 1, result[i].get_text().strip()))
+image_list = []
+
+for i in range(len(image_tag)):
+    image_list.append(image_tag[i].get_attribute('src'))
+print(image_list)
+
+for i, link in enumerate(image_list):
+    urlretrieve(link, './images/{}{}.jpg'.format(test, i + 1))
 
 # 이미지 상세창 닫기
 time.sleep(10)
 driver.find_element_by_xpath('/html/body/div[4]/div/div/div[1]/button').send_keys(Keys.ENTER)
-
-
-# image_tag = driver.find_elements_by_class_name('oCCRx')
-# #  뷰 클래스 코드 xLon9 / oCCRx _2Mc8_ / /html/body/div[4]/div/div/div[4]/div/div/div[1]/div[4]/div[1]/div[1]/span
-#
-# time.sleep(1)
-#
-# image_list = []
-#
-# for i in range(len(image_tag)):
-#     image_list.append(image_tag[i].get_attribute('src'))
-# print(image_list)
-#
-# for i, link in enumerate(image_list):
-#     urlretrieve(link, './images/{}{}.jpg'.format(test, i + 1))
